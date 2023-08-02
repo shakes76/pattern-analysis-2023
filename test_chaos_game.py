caf6278fe4cf13e9e3416931d5352278880c1903
Shakes Chandra (x14) <shekhar.chandra@uq.edu.au>
2023-08-03 09:01:42 +1000

    Working chaos game implemented

--- test_chaos_game.py
+++ test_chaos_game.py
@@ -13,16 +13,32 @@ print("Points:", points)
 #pick start position
 start = 0.1+0.5j
 
-rand_location = np.random.randint(0, n)
-print(rand_location, points[rand_location])
-vector = (points[rand_location] - start)/2.0
-new_point = start + vector
+def compute_new_rand_location(startLoc):
+    rand_location = np.random.randint(0, n)
+    #print(rand_location, points[rand_location])
+    vector = (points[rand_location] - startLoc)/2.0
+    new_point = startLoc + vector
+    return new_point, points[rand_location]
+
+next_point, rand_location = compute_new_rand_location(start)
 
 #plot
 import matplotlib.pyplot as plt
 
 #plot
-plt.plot(np.real(points[rand_location]), np.imag(points[rand_location]), "r.")
+# plt.plot(np.real(points[rand_location]), np.imag(points[rand_location]), "r.")
+# plt.plot(np.real(start), np.imag(start), "g.")
+# plt.plot(np.real(next_point), np.imag(next_point), "b.")
+# plt.show()
+
+iterations = 1000
+
+next_point = start
+for iteration in range(iterations):
+    next_point, rand_location = compute_new_rand_location(next_point)
+    plt.plot(np.real(next_point), np.imag(next_point), "b.")
+    
+#plot
+plt.plot(np.real(points), np.imag(points), "r.")
 plt.plot(np.real(start), np.imag(start), "g.")
-plt.plot(np.real(new_point), np.imag(new_point), "b.")
 plt.show()
