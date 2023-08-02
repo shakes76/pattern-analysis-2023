aef6157e329595c95726de300a0c83bb72a8f0e0
Shakes Chandra (x14) <shekhar.chandra@uq.edu.au>
2023-08-03 09:20:17 +1000

    Completed working unit test for logistic

--- logistic/unit_population.py
+++ logistic/unit_population.py
@@ -2,3 +2,19 @@
 Unit test for computing one iteration of the logistic equation
 '''
 import numpy as np
+
+N = 30 # number of steps to compute
+x = 0.5
+Lambda = 2.3
+
+T = np.arange(N)
+X = np.zeros(N)
+
+for t in T:
+    x = Lambda*x*(1-x)
+    X[t] = x
+
+import matplotlib.pyplot as plt
+
+plt.plot(T, X, '-k')
+plt.show()
